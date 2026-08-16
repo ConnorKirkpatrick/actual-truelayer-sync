e2cca05d86d5dcec4bc28826d4554c4e1df53b9c
Connor Kirkpatrick <connorkirkpatrick3@gmail.com>
2026-08-16 15:44:57 +0100

    fxi: missing base dir

--- src/actual/actual.ts
+++ src/actual/actual.ts
@@ -12,6 +12,7 @@ export async function initActual(options: InitOptions): Promise<void> {
     serverURL: options.serverURL,
     password: options.password,
     verbose: options.verbose,
+    dataDir: './data',
   })
   await actual.downloadBudget(options.syncId)
 }
